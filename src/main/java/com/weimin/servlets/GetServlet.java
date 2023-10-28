422ef197256ccd89e7508e68adb8fb2bf4a03f52
weimin <2561394933@qq.com>
2023-10-28 15:52:47 +0800

    spring session

---
+++ src/main/java/com/weimin/servlets/GetServlet.java
@@ -0,0 +1,23 @@
+package com.weimin.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/get")
+public class GetServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String user = (String) req.getSession().getAttribute("user");
+
+        resp.getWriter().println(user == null ? "no data":user);
+    }
+}
